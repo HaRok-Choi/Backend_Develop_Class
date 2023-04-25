3ad491e89f9333707d464d83453ff64d7e08c999
Harok Choi <gkfhrwkd23@naver.com>
2023-04-25 12:26:28 +0900

    Spring Board Practice

---
+++ Spring-boot-basic/board/src/test/java/com/harok/board/BoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.harok.board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
